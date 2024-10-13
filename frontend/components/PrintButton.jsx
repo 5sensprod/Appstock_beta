84ab01ce05fa31a25e86c4ea5131f1f37b592186
chris <5sensprod@gmail.com>
2024-10-13 06:32:31 +0200

    ajout tailwind pretteir

--- frontend/components/PrintButton.jsx
+++ frontend/components/PrintButton.jsx
@@ -2,7 +2,14 @@
 import React from 'react'
 
 const PrintButton = ({ onClick }) => {
-  return <button onClick={onClick}>Imprimer</button>
+  return (
+    <button
+      onClick={onClick}
+      className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+    >
+      Imprimer
+    </button>
+  )
 }
 
 export default PrintButton
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/App.css'
 import '../styles/index.css'
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
