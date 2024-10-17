543f1f6bb228fb11fa1aa53fd2e151a5e17ca8c2
chris <5sensprod@gmail.com>
2024-10-17 16:34:15 +0200

    style

--- frontend/components/ThemeWrapper.jsx
+++ frontend/components/ThemeWrapper.jsx
@@ -6,17 +6,12 @@ const ThemeWrapper = ({ children }) => {
   const { isDarkMode } = useTheme()
 
   useEffect(() => {
-    document.documentElement.classList.toggle(
-      'dark',
-      isDarkMode
-    )
+    document.documentElement.classList.toggle('dark', isDarkMode)
   }, [isDarkMode])
 
   return (
     <div className="min-h-screen bg-light-background text-light-text transition-colors duration-500 dark:bg-dark-background dark:text-dark-text">
-      <div className="mx-auto w-full max-w-screen-lg px-4">
-        {children}
-      </div>
+      <div className="mx-auto w-full max-w-screen-xl px-4">{children}</div>
     </div>
   )
 }
--- frontend/pages/labels.jsx
+++ frontend/pages/labels.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import FabricDesigner from '../components/labels/FabricDesigner'
 import ConfigForm from '../components/labels/ConfigForm'
 import LayoutGrid from '../components/labels/LayoutGrid'
-import { CanvasProvider } from '../context/CanvasContext' // Pas besoin de useCanvas ici
+import { CanvasProvider } from '../context/CanvasContext'
 
 const Labels = () => {
   return (
     <CanvasProvider>
-      <div className="mx-auto flex w-full max-w-5xl flex-col rounded-lg bg-light-background p-6 shadow-lg dark:bg-dark-background lg:flex-row">
+      <div className="mx-auto flex flex-col rounded-lg bg-light-background p-6 shadow-lg dark:bg-dark-background lg:flex-row">
         {/* Left Panel */}
-        <div className="border-r border-gray-300 p-6 lg:w-[49%]">
+        <div className="border-r border-gray-300 p-6 lg:w-3/5">
           <h2 className="mb-4 text-xl font-semibold">Aperçu de l'Étiquette</h2>
           <FabricDesigner />
         </div>
 
         {/* Right Panel */}
-        <div className="p-6 lg:w-[51%]">
+        <div className="p-6 lg:w-2/5">
           {/* Section Configuration */}
           <div className="rounded-lg border border-gray-300 bg-light-background p-4 dark:bg-dark-background">
             <h2 className="mb-4 text-xl font-semibold">Configuration</h2>
