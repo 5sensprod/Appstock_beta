c3159210eded774ce1928d04c2c0661323411d7f
chris <5sensprod@gmail.com>
2024-10-17 16:56:08 +0200

    style

--- frontend/components/labels/ZoomControl.jsx
+++ frontend/components/labels/ZoomControl.jsx
@@ -4,15 +4,19 @@ import styles from './FabricDesigner.module.css'
 export default function ZoomControl({ zoomLevel, handleZoomChange }) {
   return (
     <div className={styles.zoomControl}>
-      <label htmlFor="zoom">Zoom: {zoomLevel}x</label>
+      <label htmlFor="zoom" className="flex items-center space-x-2">
+        <span>Zoom :</span>
+        <div className="w-12 rounded border bg-gray-100 p-1 text-center">{zoomLevel}x</div>
+      </label>
       <input
         type="range"
         id="zoom"
-        min="0.5"
+        min="0.75"
         max="5"
-        step="0.1"
+        step="0.25"
         value={zoomLevel}
         onChange={handleZoomChange}
+        className="mt-2 w-full"
       />
     </div>
   )
