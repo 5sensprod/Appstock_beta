198751b006a3d1c1898190ef1a85afcd04316c89
chris <5sensprod@gmail.com>
2024-11-19 13:50:29 +0100

    prepa canvas et zoom

--- frontend/components/labels/FabricDesigner.jsx
+++ frontend/components/labels/FabricDesigner.jsx
@@ -3,18 +3,18 @@ import styles from './FabricDesigner.module.css'
 import CanvasControl from './CanvasControl'
 import Menu from './Menu'
 import ZoomControl from './ZoomControl'
-import CopyDesignButton from './CopyDesignButton'
-import PasteDesignButton from './PasteDesignButton'
-import ClearCellButton from './ClearCellButton'
+// import CopyDesignButton from './CopyDesignButton'
+// import PasteDesignButton from './PasteDesignButton'
+// import ClearCellButton from './ClearCellButton'
 
 export default function FabricDesigner() {
   return (
     <div className={styles.app}>
       <Menu />
       <div className="flex items-center space-x-2">
-        <CopyDesignButton />
+        {/* <CopyDesignButton />
         <PasteDesignButton />
-        <ClearCellButton />
+        <ClearCellButton /> */}
         <ZoomControl />
       </div>
       <CanvasControl />
--- frontend/components/labelsgenerator/LabelsContent.jsx
+++ frontend/components/labelsgenerator/LabelsContent.jsx
@@ -10,7 +10,9 @@ import ButtonReset from './buttons/ButtonReset'
 import ButtonUndo from './buttons/ButtonUndo'
 import ButtonRedo from './buttons/ButtonRedo'
 import ButtonExportPDF from './buttons/ButtonExportPDF'
-
+// import FabricDesigner from '../labels/FabricDesigner'
+import ZoomControl from '../labels/ZoomControl'
+import CanvasControl from '../labels/CanvasControl'
 const LabelsContent = () => {
   const { state, dispatch, findLinkedGroup } = useContext(GridContext)
   const { selectedCellId, cellContents, config } = state
@@ -41,6 +43,8 @@ const LabelsContent = () => {
         linkedGroup={selectedCellId ? findLinkedGroup(selectedCellId) : []} // Tableau vide si aucune cellule sélectionnée
         dispatch={dispatch} // Synchronisation avec le reducer
       />
+      <ZoomControl />
+      <CanvasControl />
     </div>
   )
 }
