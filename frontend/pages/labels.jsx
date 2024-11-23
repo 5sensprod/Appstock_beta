089b928aea5dec84f33d6d1f1d6ed48265682f10
chris <5sensprod@gmail.com>
2024-11-23 17:09:24 +0100

    ajout grid

--- frontend/pages/labels.jsx
+++ frontend/pages/labels.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { CanvasProvider } from '../context/CanvasContext'
 import ConfigForm from '../components/labels/ConfigForm'
 import FabricDesigner from '../components/labels/FabricDesigner'
+import GridManager from '../components/labelsgenerator/GridManager'
+import { GridProvider } from '../context/GridContext'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
@@ -13,54 +15,57 @@ const LabelsPage = () => {
     setIsRightPanelVisible(!isRightPanelVisible)
   }
   return (
-    <CanvasProvider>
-      <div className="mx-auto flex flex-col rounded-lg bg-light-background p-6 shadow-lg dark:bg-dark-background lg:flex-row">
-        {/* Panneau de gauche */}
-        <div
-          className={`border-r border-gray-300 p-6 transition-all duration-300 ${
-            isRightPanelVisible ? 'lg:w-3/5' : 'lg:w-full'
-          }`}
-        >
-          <h2 className="mb-4 text-xl font-semibold">Aperçu de l'Étiquette</h2>
-          <FabricDesigner />
-        </div>
+    <GridProvider>
+      <CanvasProvider>
+        <div className="mx-auto flex flex-col rounded-lg bg-light-background p-6 shadow-lg dark:bg-dark-background lg:flex-row">
+          {/* Panneau de gauche */}
+          <div
+            className={`border-r border-gray-300 p-6 transition-all duration-300 ${
+              isRightPanelVisible ? 'lg:w-3/5' : 'lg:w-full'
+            }`}
+          >
+            <h2 className="mb-4 text-xl font-semibold">Aperçu de l'Étiquette</h2>
+            <FabricDesigner />
+          </div>
 
-        {/* Bouton pour afficher/cacher le panneau droit */}
-        <div className="flex items-center justify-center">
-          <button onClick={toggleRightPanel} className="mx-2">
-            <FontAwesomeIcon
-              icon={isRightPanelVisible ? faChevronRight : faChevronLeft}
-              size="lg"
-              className="transition-colors duration-300 hover:text-blue-500"
-            />
-          </button>
-        </div>
+          {/* Bouton pour afficher/cacher le panneau droit */}
+          <div className="flex items-center justify-center">
+            <button onClick={toggleRightPanel} className="mx-2">
+              <FontAwesomeIcon
+                icon={isRightPanelVisible ? faChevronRight : faChevronLeft}
+                size="lg"
+                className="transition-colors duration-300 hover:text-blue-500"
+              />
+            </button>
+          </div>
 
-        {/* Panneau de droite */}
+          {/* Panneau de droite */}
 
-        <div
-          className={`transition-all duration-300 ${
-            isRightPanelVisible ? 'p-6 lg:w-2/5' : 'w-0 overflow-hidden p-0'
-          }`}
-        >
-          {isRightPanelVisible && (
-            <>
-              <div className="rounded-lg border border-gray-300 bg-light-background p-4 dark:bg-dark-background">
-                <h2 className="mb-4 text-xl font-semibold">Configuration</h2>
-                <ConfigForm />
-              </div>
-
-              {/* Section Disposition sur A4 */}
-              <div className="mt-6 rounded-lg border border-gray-300 bg-light-background p-4 dark:bg-dark-background">
-                <div className="flex items-center justify-between">
-                  <h2 className="mb-4 text-xl font-semibold">Disposition sur A4</h2>
+          <div
+            className={`transition-all duration-300 ${
+              isRightPanelVisible ? 'p-6 lg:w-2/5' : 'w-0 overflow-hidden p-0'
+            }`}
+          >
+            {isRightPanelVisible && (
+              <>
+                <div className="rounded-lg border border-gray-300 bg-light-background p-4 dark:bg-dark-background">
+                  <h2 className="mb-4 text-xl font-semibold">Configuration</h2>
+                  <ConfigForm />
                 </div>
-              </div>
-            </>
-          )}
+
+                {/* Section Disposition sur A4 */}
+                <div className="mt-6 rounded-lg border border-gray-300 bg-light-background p-4 dark:bg-dark-background">
+                  <div className="flex items-center justify-between">
+                    <h2 className="mb-4 text-xl font-semibold">Disposition sur A4</h2>
+                  </div>
+                  <GridManager />
+                </div>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </CanvasProvider>
+      </CanvasProvider>
+    </GridProvider>
   )
 }
 
