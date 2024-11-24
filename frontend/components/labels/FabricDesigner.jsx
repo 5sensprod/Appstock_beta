e1aacefc379822c07766cdb777c41e48db442971
chris <5sensprod@gmail.com>
2024-11-24 15:13:01 +0100

    reboot

--- frontend/components/labels/FabricDesigner.jsx
+++ frontend/components/labels/FabricDesigner.jsx
@@ -4,10 +4,12 @@ import CanvasControl from './CanvasControl copy'
 // import CanvasControl from './CanvasControl'
 import Menu from './Menu'
 import ZoomControl from './ZoomControl'
+import MenuCell from '../labelsgenerator/MenuCell'
 
 export default function FabricDesigner() {
   return (
     <div className={styles.app}>
+      <MenuCell />
       <Menu />
       <div className="flex items-center space-x-2">
         <ZoomControl />
