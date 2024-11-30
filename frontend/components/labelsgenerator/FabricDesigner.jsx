6e9c124df06a103cbfcb32df80a4e88a5bb57792
chris <5sensprod@gmail.com>
2024-11-30 14:50:37 +0100

    reorder

--- frontend/components/labelsgenerator/FabricDesigner.jsx
+++ frontend/components/labelsgenerator/FabricDesigner.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import styles from './FabricDesigner.module.css'
 import CanvasControl from './CanvasControl'
-// import CanvasControl from './CanvasControl'
 import Menu from './Menu'
 import ZoomControl from './ZoomControl'
-import MenuCell from '../labelsgenerator/MenuCell'
+import MenuCell from './MenuCell'
 
 export default function FabricDesigner() {
   return (
